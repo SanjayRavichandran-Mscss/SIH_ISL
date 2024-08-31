7fd0f0c8ad65139f4ab8feffe49e35c7caa39c3a
SanjayRavichandran-Mscss <117557388+SanjayRavichandran-Mscss@users.noreply.github.com>
2024-08-31 17:46:21 +0530

    remove video element

--- src/App.jsx
+++ src/App.jsx
@@ -66,22 +66,7 @@ export default function App() {
 
 
 
-    {/*  <div className="avatar" style={{ width: '30%', height: '100vh' }}> */}
-        {/* MP4 Video Player */}
-     {/*     <video className='vid'
-          style={{ width: '100%', height: '100%' }}
-          // controls
-          autoPlay
-          loop
-          muted
-        >
-          <source src="src/assets/R1.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-         
-      
-      </div>
-*/}
+   
   
      
     </div>
